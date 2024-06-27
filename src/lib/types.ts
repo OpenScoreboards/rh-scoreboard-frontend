9659e6af886194c749351d4bfb6a437db3c6a73d
Tyson <tyson@clugg.net>
2024-06-27 04:06:05 +0000

    First cut of standard types

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,2 +1,42 @@
-
 export type connectionStateType = 'idle' | 'good' | 'warn' | 'fail';
+
+export interface ClockInterface {
+    last_state_change: number;
+    last_time_remaining: number;
+    state: "Running" | "Stopped";
+}
+
+export class ClockBase implements ClockInterface {
+    last_state_change: number;
+    last_time_remaining: number;
+    state: "Running" | "Stopped";
+
+    constructor() {
+        this.last_state_change = 0;
+        this.last_time_remaining = 0;
+        this.state = "Stopped"
+    }
+}
+
+export interface TeamScoreInterface {
+	score: number;
+	team_fouls: number;
+	timeout_requested: boolean;
+	fouls_accumulated: boolean;
+
+	scoreIncrement: (add?: number) => void;
+	scoreDecrement: (subtract?: number) => void;
+
+	foulsIncrement: (add?: number) => void;
+	foulsDecrement: (subtract?: number) => void;
+
+    toggleTimeout: () => void;
+    toggleFouls: () => void;
+}
+
+export interface GameInterface {
+    home: TeamScoreInterface;
+    away: TeamScoreInterface;
+    game_clock: ClockInterface;
+    shot_clock: ClockInterface;
+}
